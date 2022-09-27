1c21aa78b5d8e8d33bbde98a6289e851dc4459f7
Ilya <sia212007@gmail.com>
2022-09-27 15:51:27 +0300

    v1.0

---
+++ src/components/app/app.js
@@ -0,0 +1,120 @@
+import { Component } from 'react'
+
+import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel'
+import AppFilter from '../app-filter/app-filter'
+import EmployeesList from '../employees-list/employees-list'
+import EmployeesAddForm from '../employees-add-form/employees-add-form'
+
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            employees: [
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
+            ],
+            term: '', 
+            filter: 'all'
+        }
+        this.maxId = 4
+    }
+
+    deleteItem = (id) => {
+        this.setState(({employees}) => {
+            return {
+                employees: employees.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            rise: false,
+            id: this.maxId++
+        }
+        this.setState(({employees}) => {
+            const newArr = [...employees, newItem];
+            return {
+                employees: newArr
+            }
+        });
+    }
+
+    onToggleProp = (id, prop) => {
+        this.setState(({employees}) => ({
+            employees: employees.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item
+            })
+        }))
+    }
+
+    searchEmployee = (items, term) => {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise)
+            case 'moreThan1000':
+                return items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
+
+    render() {
+    const {employees, term, filter} = this.state;
+        const employeesAmount = this.state.employees.length;
+        const employeesIncreaseAmount = this.state.employees.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmployee(employees, term), filter);
+
+
+        return (
+            <div className="app">
+                <AppInfo employeesAmount={employeesAmount} employeesIncreaseAmount={employeesIncreaseAmount}/>
+    
+                <div className="search-panel">
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
+                </div>
+                
+                <EmployeesList 
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,24 @@
+import EmployeesListItem from '../employees-list-item/employees-list-item'
+import './employees-list.css'
+
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item
+        return (
+            <EmployeesListItem 
+                key={id} 
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+                onToggleProp={(event) => onToggleProp(id, event.currentTarget.getAttribute('data-toggle'))}
+            />
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList
